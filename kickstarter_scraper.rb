7b8291939e27cfeffd9843ba753fd11a3b6ca7de
Vincent Servio <vincentservio@gmail.com>
2020-02-27 20:06:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
+ binding.pry 
   projects = {}
  
   kickstarter.css("li.project.grid_4").each do |project|
